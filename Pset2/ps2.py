219a05f23b50885f7c9d0f8a04a66ece4f465432
Mohammad Tolooei <mamadlarestan@gmail.com>
2026-07-30 00:57:50 +0330

    feat(robot):implement Robot base class
    Added Robot class with support for:
    
    - random initial position and direction
    - store and update robot position and direction
    - defining updatePositionAndClean() as an abstract interface

--- Pset2/ps2.py
+++ Pset2/ps2.py
@@ -175,7 +175,10 @@ class Robot(object):
         room:  a RectangularRoom object.
         speed: a float (speed > 0)
         """
-        raise NotImplementedError
+        self.speed = speed
+        self.room = room
+        self.position = room.getRandomPosition()
+        self.direction = random.randint(0, 360-1)
 
     def getRobotPosition(self):
         """
@@ -183,7 +186,7 @@ class Robot(object):
 
         returns: a Position object giving the robot's position.
         """
-        raise NotImplementedError
+        return self.position
 
     def getRobotDirection(self):
         """
@@ -192,7 +195,7 @@ class Robot(object):
         returns: an integer d giving the direction of the robot as an angle in
         degrees, 0 <= d < 360.
         """
-        raise NotImplementedError
+        return self.direction
 
     def setRobotPosition(self, position):
         """
@@ -200,7 +203,7 @@ class Robot(object):
 
         position: a Position object.
         """
-        raise NotImplementedError
+        self.position = position
 
     def setRobotDirection(self, direction):
         """
@@ -208,8 +211,8 @@ class Robot(object):
 
         direction: integer representing an angle in degrees
         """
-        raise NotImplementedError
-
+        self.direction=direction
+        
     def updatePositionAndClean(self):
         """
         Simulate the passage of a single time-step.
